5402238c6a319d3db98a8239ae7336294fa85de5
TeAmP0is0N <rohitrathore.imh55@gmail.com>
2019-08-17 19:32:54 +0530

    Explanation added

--- week 3/create and traverse a linked list.c
+++ week 3/create and traverse a linked list.c
@@ -1,5 +1,41 @@
 /**
  * C program to create and traverse a Linked List
+ How to create a linked list?
+Step by step descriptive logic to create a linked list.
+
+1. The first step of creating linked list of n nodes starts from defining node structure. We need a custom type to store our data and location of next linked node. Let us define our custom node structure
+struct node {
+    int data;
+    struct node *next;
+};
+Where data is the data you want to store in list. *next is pointer to the same structure type. The *next will store location of next node if exists otherwise NULL.
+
+Note: The node structure may vary based on your requirement. You can also have user defined types as node data section.
+
+2. Declare a pointer to node type variable to store link of first node of linked list. Say struct node *head;.
+Note: You can also declare variable of node type along with node structure definition.
+
+3. Input number of nodes to create from user, store it in some variable say n.
+4. Declare two more helper variable of node type, say struct node *newNode, *temp;.
+5. If n > 0 then, create our first node i.e. head node. Use dynamic memory allocation to allocate memory for a node. Say head = (struct node*)malloc(sizeof(struct node));.
+6. If there is no memory to allocate for head node i.e. head == NULL. Then print some error message and terminate program, otherwise move to below step.
+7. Input data from user and assign to head using head->data = data;.
+8. At first head node points to NULL. Hence, assign head->next = NULL;.
+9. Now, we are done with head node we should move to creation of other nodes. Copy reference of head to some other temporary variable, say temp = head;. We will use temp to store reference of previous node.
+10. Allocate memory and assign memory reference to newNode, say newNode = (struct node*)malloc(sizeof(node));.
+11. If memory got allocated successfully then read data from user and assign to data section of new node. Say newNode->data = data;.
+12. Make sure new node points to NULL.
+13. Now link previous node with newly created node i.e. temp->next = newNode;.
+14. Make current node as previous node using temp = temp->next;.
+15. Repeat step 10-14 for remaining n - 2 other nodes.
+How to traverse a linked list?
+Step by step descriptive logic to traverse a linked list.
+
+1. Create a temporary variable for traversing. Assign reference of head node to it, say temp = head.
+2. Repeat below step till temp != NULL.
+3. temp->data contains the current node data. You can print it or can perform some calculation on it.
+4. Once done, move to next node using temp = temp->next;.
+5. Go back to 2nd step.
  */
 
 #include <stdio.h>
